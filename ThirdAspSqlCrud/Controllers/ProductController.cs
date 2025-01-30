572287125cfdf9d569882c68925d967d4b87d11b
ASUS <ASUS@DESKTOP-NB7MT4D>
2025-01-30 18:50:18 +0400

    Task Img

--- ThirdAspSqlCrud/Controllers/ProductController.cs
+++ ThirdAspSqlCrud/Controllers/ProductController.cs
@@ -30,18 +30,43 @@ namespace ThirdAspSqlCrud.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<ActionResult> Add(ProductAddViewModel vm)
+        public async Task<ActionResult> Add(ProductAddViewModel vm, IFormFile imgFile)
         {
             if (ModelState.IsValid)
             {
+                if (imgFile == null || imgFile.Length == 0)
+                {
+                    ModelState.AddModelError("ImgFile", "No file selected.");
+                    return View(vm); //file secilmese meesage
+                }
+
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                var filePath = Path.Combine(uploadsFolder, imgFile.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await imgFile.CopyToAsync(stream);
+                }
+
+                vm.Product.ImgLink = "/images/" + imgFile.FileName;
+
                 await _productService.AddAsync(vm.Product);
                 return RedirectToAction("Index");
             }
+
             return View(vm);
         }
+
         [HttpGet]
         public async Task<ActionResult> Update(int id)
         {
+
             var product = await _productService.GetWithId(id);
             if (product == null)
             {
@@ -55,10 +80,33 @@ namespace ThirdAspSqlCrud.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Update(int id, ProductEditViewModel vm)
+        public async Task<ActionResult> Update(int id, ProductEditViewModel vm, IFormFile img)
         {
             if (ModelState.IsValid)
             {
+                var prod = await _productService.GetWithId(id);
+                if (prod == null) {
+                    return NotFound();
+                }
+                if(img != null && img.Length > 0)
+                {
+                    var fileUp = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+
+                    if (!Directory.Exists(fileUp))
+                    {
+                        Directory.CreateDirectory(fileUp);
+                    }
+
+                    var filePath = Path.Combine(fileUp, img.FileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await img.CopyToAsync(stream);
+                    }
+
+                    prod.ImgLink = "/images/" + img.FileName;
+                    vm.Product.ImgLink = prod.ImgLink;
+                }
                 await _productService.Update(id, vm);
                 return RedirectToAction("Index");
             }
